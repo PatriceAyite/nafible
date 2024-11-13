615f180b4e6855459bbef2efc8cc00ee7ff40dd1
Patrice Ayite <patriceayite72@gmail.com>
2024-11-13 11:06:19 +0000

    new update

---
+++ NafiProject/src/app/components/head-page/head-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './head-page.component.html',
+  styleUrl: './head-page.component.css'
+})
+export class HeadPageComponent {
+
+}
