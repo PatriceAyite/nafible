ef30320f013b1bb553df304b160b144385a76eb8
Patrice Ayite <patriceayite72@gmail.com>
2024-11-20 08:17:21 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	NafiProject/src/app/components/head-page/head-page.component.html
    #	NafiProject/src/app/components/home-page/home-page.component.css
    #	NafiProject/src/app/components/home-page/home-page.component.html

--- NafiProject/src/app/app.routes.ts
+++ NafiProject/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import {HomePageComponent} from './components/home-page/home-page.component';
 import {NewProviderComponent} from './components/new-provider/new-provider.component';
 import {BookingPageComponent} from './components/booking-page/booking-page.component';
+import {ManagerBookingComponent} from './dashboard/manager-booking/manager-booking.component';
 
 export const routes: Routes = [
   { path: 'home', component:HomePageComponent },
   { path: 'new-provider', component:NewProviderComponent},
-  { path: 'booking', component:BookingPageComponent},
+  { path: 'dashboard', component:ManagerBookingComponent},
   { path: '', component: HomePageComponent },
 ];
