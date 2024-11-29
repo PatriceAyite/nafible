29eb3017ab239cef7b4a1e39477640425cd2bc25
Patrice Ayite <patriceayite72@gmail.com>
2024-11-29 17:52:36 +0000

    New Update

---
+++ NafiProject/src/app/components/manager-booking/manager-booking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-booking',
+  standalone: true,
+  imports: [],
+  templateUrl: './manager-booking.component.html',
+  styleUrl: './manager-booking.component.css'
+})
+export class ManagerBookingComponent {
+
+}
